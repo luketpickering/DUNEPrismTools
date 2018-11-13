39b41e79e77ae9cedb0ae4995c26c1b907a18a1e
Luke Pickering <luketpickering@googlemail.com>
2018-11-13 17:40:51 -0600

    Adds summary tree of parent decay points.

--- plots/sanity_macros/MakeSlimFluxSanityPlots.C
+++ plots/sanity_macros/MakeSlimFluxSanityPlots.C
@@ -3,7 +3,7 @@
 #include "TH2D.h"
 
 void MakeSlimFluxSanityPlots(char const *add, bool append = false,
-                             char const *subdir = "") {
+                             char const *subdir = "", bool maketree = true) {
   TChain *dk2nuTree_lite = new TChain("dk2nuTree_lite");
   dk2nuTree_lite->Add(add);
 
@@ -33,11 +33,33 @@ void MakeSlimFluxSanityPlots(char const *add, bool append = false,
                        "decay_nimpwt", "GOFF");
 
   TH2D *p_xy_imp_fine = new TH2D("p_xy_imp_fine", "", 40, -20, 20, 40, -20, 20);
-  dk2nuTree_lite->Draw("decay_vy:decay_vx >> p_xy_imp_fine",
-                       "decay_nimpwt", "GOFF");
-  TH2D *p_yz_imp_fine = new TH2D("p_yz_imp_fine", "", 1000, 0, 10000, 60, -30, 30);
-  dk2nuTree_lite->Draw("decay_vy:decay_vz >> p_yz_imp_fine",
-                       "decay_nimpwt", "GOFF");
+  dk2nuTree_lite->Draw("decay_vy:decay_vx >> p_xy_imp_fine", "decay_nimpwt",
+                       "GOFF");
+  TH2D *p_yz_imp_fine =
+      new TH2D("p_yz_imp_fine", "", 1000, 0, 10000, 60, -30, 30);
+  dk2nuTree_lite->Draw("decay_vy:decay_vz >> p_yz_imp_fine", "decay_nimpwt",
+                       "GOFF");
+
+  if (maketree) {
+    TTree *stree = new TTree("sum_tree", "");
+    double parent_x, parent_y, parent_z, impweight;
+    stree->Branch("parent_x", &parent_x);
+    stree->Branch("parent_y", &parent_y);
+    stree->Branch("parent_z", &parent_z);
+    stree->Branch("impweight", &impweight);
+
+    dk2nuTree_lite->SetBranchAddress("decay_vx", &parent_x);
+    dk2nuTree_lite->SetBranchAddress("decay_vy", &parent_y);
+    dk2nuTree_lite->SetBranchAddress("decay_vz", &parent_z);
+    dk2nuTree_lite->SetBranchAddress("decay_nimpwt", &impweight);
+
+    size_t npars = dk2nuTree_lite->GetEntries();
+    for (size_t it = 0; it < npars, ++it) {
+      dk2nuTree_lite->GetEntry(it);
+      stree->Fill();
+    }
+    stree->SetDirectory(oupD);
+  }
 
   p_xy->SetDirectory(oupD);
   p_yz->SetDirectory(oupD);
