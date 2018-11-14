32c1587157087ff665565d2cbcffeb68a91131f3
Luke Pickering <luketpickering@googlemail.com>
2018-11-14 13:07:13 -0600

    Adds focussing shift macros.

--- scripts/flux_scripts/ProcessRunFluxSim.sh
+++ scripts/flux_scripts/ProcessRunFluxSim.sh
@@ -2,20 +2,32 @@
 
 #Test variations
 
+# for i in nu nubar; do
+#   for j in X Y; do
+#     for k in Horn1 Horn2 DecayPipe; do
+#      ${DUNEPRISMTOOLSROOT}/scripts/flux_scripts/FarmG4LBNE_PPFX_Makedk2nuLite.sh \
+#       -m ${DUNEPRISMTOOLSROOT}/configs/g4lbnf_macros/OptimizedEngineeredNov2017Review_${k}${j}Shift_${i}mode.mac \
+#       -p test_alignment/v3r5p4/QGSP_BERT/OptimizedEngineeredNov2017Review/${k}${j}Shift_${i}/neutrino \
+#       --number-of-jobs 1 \
+#       --expected-disk 1GB \
+#       --expected-mem 1GB \
+#       --expected-walltime 4h
+#     done
+#   done
+# done
+
 for i in nu nubar; do
-  for j in X Y; do
-    for k in Horn1 Horn2 DecayPipe; do
+  for j in p1; do
+    for k in WL HC DPR; do
      ${DUNEPRISMTOOLSROOT}/scripts/flux_scripts/FarmG4LBNE_PPFX_Makedk2nuLite.sh \
-      -m ${DUNEPRISMTOOLSROOT}/configs/g4lbnf_macros/OptimizedEngineeredNov2017Review_${k}${j}Shift_${i}mode.mac \
-      -p test_alignment/v3r5p4/QGSP_BERT/OptimizedEngineeredNov2017Review/${k}${j}Shift_${i}/neutrino \
-      --number-of-jobs 1 \
+      -m ${DUNEPRISMTOOLSROOT}/configs/g4lbnf_macros/OptimizedEngineeredNov2017Review_${i}mode_${k}${j}.mac \
+      -p test_alignment/v3r5p4/QGSP_BERT/OptimizedEngineeredNov2017Review/${k}${j}/${i} \
+      --number-of-jobs 1000 \
       --expected-disk 1GB \
       --expected-mem 1GB \
-      --expected-walltime 4h
+      --expected-walltime 5h
     done
-  done
 done
-
 
 # ${DUNEPRISMTOOLSROOT}/scripts/flux_scripts/FarmG4LBNE_PPFX_Makedk2nuLite.sh \
 #   -m ${DUNEPRISMTOOLSROOT}/configs/g4lbnf_macros/OptimizedEngineeredNov2017Review_numode.mac \
@@ -25,10 +37,10 @@ done
 #   --expected-mem 1GB \
 #   --expected-walltime 4h
 #
-# ${DUNEPRISMTOOLSROOT}/scripts/flux_scripts/FarmG4LBNE_PPFX_Makedk2nuLite.sh \
-#   -m ${DUNEPRISMTOOLSROOT}/configs/g4lbnf_macros/OptimizedEngineeredNov2017Review_nubarmode.mac \
-#   -p nominal_2.5E8_wppfx/v3r5p4/QGSP_BERT/OptimizedEngineeredNov2017Review/antineutrino \
-#   --number-of-jobs 2500 --PPFX \
-#   --expected-disk 1GB \
-#   --expected-mem 1GB \
-#   --expected-walltime 4h
+${DUNEPRISMTOOLSROOT}/scripts/flux_scripts/FarmG4LBNE_PPFX_Makedk2nuLite.sh \
+  -m ${DUNEPRISMTOOLSROOT}/configs/g4lbnf_macros/OptimizedEngineeredNov2017Review_nubarmode.mac \
+  -p nominal_2.5E8_wppfx/v3r5p4/QGSP_BERT/OptimizedEngineeredNov2017Review/antineutrino \
+  --number-of-jobs 1500 --PPFX \
+  --expected-disk 1GB \
+  --expected-mem 1GB \
+  --expected-walltime 5h
