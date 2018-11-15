a653d6b8709405f7a82d02115d65596fe29c5dfb
Luke Pickering <luketpickering@googlemail.com>
2018-11-15 16:08:43 -0600

    bad fix for bad newline...

--- scripts/flux_scripts/ProcessFDFluxPredictions.sh
+++ scripts/flux_scripts/ProcessFDFluxPredictions.sh
@@ -3,11 +3,9 @@
 BINNING_DESCRIPTOR_FITS="0_10:0.025"
 BINNING_DESCRIPTOR_UNCERTS="0,0.5,1_3:0.25,3_4:0.5,4_10:1,10_20:2"
 
-dp_BuildFluxes -i "/pnfs/dune/persistent/users/picker24/nominal_1.5E8_wppfx/v3r5p4/QGSP_BERT/OptimizedEngineeredNov2017Review/neutrino/dk2nulite/*.root"
--x -12.90_12.90:25.80 -h 2260 -vb ${BINNING_DESCRIPTOR_FITS} -z 128700000 -L --PPFX -o /pnfs/dune/persistent/users/picker24/nominal_1.5E8_wppfx/DUNEPrismFluxes/FD_nu/fit_binning/FD_nu_OptimizedEngineeredNov2017Review_fit_binning_wppfx.root
+dp_BuildFluxes -i "/pnfs/dune/persistent/users/picker24/nominal_1.5E8_wppfx/v3r5p4/QGSP_BERT/OptimizedEngineeredNov2017Review/neutrino/dk2nulite/*.root" -x -12.90_12.90:25.80 -h 2260 -vb ${BINNING_DESCRIPTOR_FITS} -z 128700000 -L --PPFX -o /pnfs/dune/persistent/users/picker24/nominal_1.5E8_wppfx/DUNEPrismFluxes/FD_nu/fit_binning/FD_nu_OptimizedEngineeredNov2017Review_fit_binning_wppfx.root
 
-dp_BuildFluxes -i "/pnfs/dune/persistent/users/picker24/nominal_1.5E8_wppfx/v3r5p4/QGSP_BERT/OptimizedEngineeredNov2017Review/neutrino/dk2nulite/*.root"
--x -12.90_12.90:25.80 -h 2260 -vb ${BINNING_DESCRIPTOR_UNCERTS} -z 128700000 -L --PPFX -o /pnfs/dune/persistent/users/picker24/nominal_1.5E8_wppfx/DUNEPrismFluxes/FD_nu/uncert_binning/FD_nu_OptimizedEngineeredNov2017Review_uncert_binning_wppfx.root
+dp_BuildFluxes -i "/pnfs/dune/persistent/users/picker24/nominal_1.5E8_wppfx/v3r5p4/QGSP_BERT/OptimizedEngineeredNov2017Review/neutrino/dk2nulite/*.root" -x -12.90_12.90:25.80 -h 2260 -vb ${BINNING_DESCRIPTOR_UNCERTS} -z 128700000 -L --PPFX -o /pnfs/dune/persistent/users/picker24/nominal_1.5E8_wppfx/DUNEPrismFluxes/FD_nu/uncert_binning/FD_nu_OptimizedEngineeredNov2017Review_uncert_binning_wppfx.root
 
 dp_BuildFluxes -i "/pnfs/dune/persistent/users/picker24/nominal_2.5E8_wppfx/v3r5p4/QGSP_BERT/OptimizedEngineeredNov2017Review/antineutrino/dk2nulite/*.root" -x -12.90_12.90:25.80 -h 2260 -vb ${BINNING_DESCRIPTOR_FITS} -z 128700000 -L --PPFX -o /pnfs/dune/persistent/users/picker24/nominal_2.5E8_wppfx/DUNEPrismFluxes/FD_nubar/fit_binning/FD_nubar_OptimizedEngineeredNov2017Review_fit_binning_wppfx.root
 
