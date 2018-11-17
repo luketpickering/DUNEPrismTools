21878a3f4d8c7babb02858a0db7911a88035d68a
Luke Pickering <luketpickering@googlemail.com>
2018-11-17 17:04:24 -0600

    Alters flux building grid steering scripts.

--- scripts/flux_scripts/ProcessFDFluxPredictions.sh
+++ scripts/flux_scripts/ProcessFDFluxPredictions.sh
@@ -1,25 +1,14 @@
 # !/bin/bash
 
-BINNING_DESCRIPTOR_FITS="0_10:0.025"
-BINNING_DESCRIPTOR_UNCERTS="0,0.5,1_3:0.25,3_4:0.5,4_10:1,10_20:2"
+dp_BuildFluxes -i "/pnfs/dune/persistent/users/picker24/nominal_1.5E8_wppfx/v3r5p4/QGSP_BERT/OptimizedEngineeredNov2017Review/neutrino/dk2nulite/*.root" --fhicl build_FD_fluxes_numode_ppfx.fcl -o FD_nu_OptimizedEngineeredNov2017Review_uncert_binning_wppfx.root
 
-dp_BuildFluxes -i "/pnfs/dune/persistent/users/picker24/nominal_1.5E8_wppfx/v3r5p4/QGSP_BERT/OptimizedEngineeredNov2017Review/neutrino/dk2nulite/*.root" -x -12.90_12.90:25.80 -h 2260 -vb ${BINNING_DESCRIPTOR_FITS} -z 128700000 -L --PPFX -o FD_nu_OptimizedEngineeredNov2017Review_fit_binning_wppfx.root
-
-dp_BuildFluxes -i "/pnfs/dune/persistent/users/picker24/nominal_1.5E8_wppfx/v3r5p4/QGSP_BERT/OptimizedEngineeredNov2017Review/neutrino/dk2nulite/*.root" -x -12.90_12.90:25.80 -h 2260 -vb ${BINNING_DESCRIPTOR_UNCERTS} -z 128700000 -L --PPFX -o FD_nu_OptimizedEngineeredNov2017Review_uncert_binning_wppfx.root
-
-dp_BuildFluxes -i "/pnfs/dune/persistent/users/picker24/nominal_2.5E8_wppfx/v3r5p4/QGSP_BERT/OptimizedEngineeredNov2017Review/antineutrino/dk2nulite/*.root" -x -12.90_12.90:25.80 -h 2260 -vb ${BINNING_DESCRIPTOR_FITS} -z 128700000 -L --PPFX -o FD_nubar_OptimizedEngineeredNov2017Review_fit_binning_wppfx.root
-
-dp_BuildFluxes -i "/pnfs/dune/persistent/users/picker24/nominal_2.5E8_wppfx/v3r5p4/QGSP_BERT/OptimizedEngineeredNov2017Review/antineutrino/dk2nulite/*.root" -x -12.90_12.90:25.80 -h 2260 -vb ${BINNING_DESCRIPTOR_UNCERTS} -z 128700000 -L --PPFX -o FD_nubar_OptimizedEngineeredNov2017Review_uncert_binning_wppfx.root
-
+dp_BuildFluxes -i "/pnfs/dune/persistent/users/picker24/nominal_2.5E8_wppfx/v3r5p4/QGSP_BERT/OptimizedEngineeredNov2017Review/antineutrino/dk2nulite/*.root" --fhicl build_FD_fluxes_nubarmode_ppfx.fcl -o FD_nubar_OptimizedEngineeredNov2017Review_uncert_binning_wppfx.root
 
 for i in nu nubar; do
   for j in p1; do
     for k in WL HC DPR; do
 
-    dp_BuildFluxes -i "/pnfs/dune/persistent/users/picker24/Focussing/v3r5p4/QGSP_BERT/OptimizedEngineeredNov2017Review/${k}${j}/${i}/dk2nulite/*.root" -x -12.90_12.90:25.80 -h 2260 -vb ${BINNING_DESCRIPTOR_FITS} -z 128700000 -L -o FD_${i}_OptimizedEngineeredNov2017Review_fit_binning_${k}${j}.root
-
-    dp_BuildFluxes -i "/pnfs/dune/persistent/users/picker24/Focussing/v3r5p4/QGSP_BERT/OptimizedEngineeredNov2017Review/${k}${j}/${i}/dk2nulite/*.root" -x -12.90_12.90:25.80 -h 2260 -vb ${BINNING_DESCRIPTOR_UNCERTS} -z 128700000 -L -o FD_${i}_OptimizedEngineeredNov2017Review_uncert_binning_${k}${j}.root
-
+    dp_BuildFluxes -i "/pnfs/dune/persistent/users/picker24/Focussing/v3r5p4/QGSP_BERT/OptimizedEngineeredNov2017Review/${k}${j}/${i}/dk2nulite/*.root" --fhicl build_FD_fluxes_${i}mode_ppfx.fcl -o FD_${i}_OptimizedEngineeredNov2017Review_uncert_binning_${k}${j}.root
 
     done
   done
--- scripts/flux_scripts/ProcessFluxVarJobs.sh
+++ scripts/flux_scripts/ProcessFluxVarJobs.sh
@@ -1,64 +1,31 @@
 # !/bin/bash
 
-BINNING_DESCRIPTOR_FITS="0_10:0.025"
-BINNING_DESCRIPTOR_UNCERTS="0,0.5,1_3:0.25,3_4:0.5,4_10:1,10_20:2"
-
-<<<<<<< Updated upstream
-#### With PPFX
-
 #nu
 ${DUNEPRISMTOOLSROOT}/scripts/flux_scripts/FarmBuildFluxJobs.sh \
    --expected-walltime 2h --expected-disk 2GB \
-   --expected-mem 512MB -b ${BINNING_DESCRIPTOR_UNCERTS} \
-   -p nominal_1.5E8_wppfx/DUNEPrismFluxes/ND_nu/uncert_binning -d 57400 \
+   --expected-mem 512MB -F build_ND_fluxes_numode_ppfx.fcl \
+   -p nominal_1.5E8_wppfx/DUNEPrismFluxes/ND_nu/uncert_binning \
    -i /pnfs/dune/persistent/users/picker24/nominal_1.5E8_wppfx/v3r5p4/QGSP_BERT/OptimizedEngineeredNov2017Review/neutrino/dk2nulite \
-   -n 20 -D -W " -x -0.25_45.25:0.5 -h 300 " -f -X
-
-${DUNEPRISMTOOLSROOT}/scripts/flux_scripts/FarmBuildFluxJobs.sh \
-   --expected-walltime 2h --expected-disk 2GB \
-   --expected-mem 512MB -b ${BINNING_DESCRIPTOR_FITS} \
-   -p nominal_1.5E8_wppfx/DUNEPrismFluxes/ND_nu/fit_binning -d 57400 \
-   -i /pnfs/dune/persistent/users/picker24/nominal_1.5E8_wppfx/v3r5p4/QGSP_BERT/OptimizedEngineeredNov2017Review/neutrino/dk2nulite \
-   -n 20 -D -W " -x -0.25_45.25:0.5 -h 300 " -f -X
+   -n 20 -f
 
 #nubar
 ${DUNEPRISMTOOLSROOT}/scripts/flux_scripts/FarmBuildFluxJobs.sh \
    --expected-walltime 2h --expected-disk 2GB \
-   --expected-mem 512MB -b ${BINNING_DESCRIPTOR_UNCERTS} \
-   -p nominal_2.5E8_wppfx/DUNEPrismFluxes/ND_nubar/uncert_binning -d 57400 \
+   --expected-mem 512MB -F build_ND_fluxes_nubarmode_ppfx.fcl \
+   -p nominal_2.5E8_wppfx/DUNEPrismFluxes/ND_nubar/uncert_binning  \
    -i /pnfs/dune/persistent/users/picker24/nominal_2.5E8_wppfx/v3r5p4/QGSP_BERT/OptimizedEngineeredNov2017Review/antineutrino/dk2nulite \
-   -n 20 -D -W " -x -0.25_45.25:0.5 -h 300 " -f -X
+   -n 20 -f
 
-${DUNEPRISMTOOLSROOT}/scripts/flux_scripts/FarmBuildFluxJobs.sh \
-   --expected-walltime 2h --expected-disk 2GB \
-   --expected-mem 512MB -b ${BINNING_DESCRIPTOR_FITS} \
-   -p nominal_2.5E8_wppfx/DUNEPrismFluxes/ND_nubar/fit_binning -d 57400 \
-   -i /pnfs/dune/persistent/users/picker24/nominal_2.5E8_wppfx/v3r5p4/QGSP_BERT/OptimizedEngineeredNov2017Review/antineutrino/dk2nulite \
-   -n 20 -D -W " -x -0.25_45.25:0.5 -h 300 " -f -X
-
-=======
->>>>>>> Stashed changes
 #With focussing
 for i in nu nubar; do
   for j in p1; do
     for k in WL HC DPR; do
       ${DUNEPRISMTOOLSROOT}/scripts/flux_scripts/FarmBuildFluxJobs.sh \
          --expected-walltime 2h --expected-disk 2GB \
-         --expected-mem 512MB -b ${BINNING_DESCRIPTOR_UNCERTS} \
-         -p Focussing/DUNEPrismFluxes/ND_${i}/${k}${j}/uncert_binning -d 57400 \
+         --expected-mem 512MB -F build_ND_fluxes_${i}mode.fcl \
+         -p Focussing/DUNEPrismFluxes/ND_${i}/${k}${j}/uncert_binning \
          -i /pnfs/dune/persistent/users/picker24/Focussing/v3r5p4/QGSP_BERT/OptimizedEngineeredNov2017Review/${k}${j}/${i}/dk2nulite \
-         -n 20 -D -W " -x -0.25_45.25:0.5 -h 300 " -f
-
-      ${DUNEPRISMTOOLSROOT}/scripts/flux_scripts/FarmBuildFluxJobs.sh \
-         --expected-walltime 2h --expected-disk 2GB \
-         --expected-mem 512MB -b ${BINNING_DESCRIPTOR_FITS} \
-         -p Focussing/DUNEPrismFluxes/ND_${i}/${k}${j}/fit_binning -d 57400 \
-         -i /pnfs/dune/persistent/users/picker24/Focussing/v3r5p4/QGSP_BERT/OptimizedEngineeredNov2017Review/${k}${j}/${i}/dk2nulite \
-         -n 20 -D -W " -x -0.25_45.25:0.5 -h 300 " -f
+         -n 20 -f
     done
   done
 done
-<<<<<<< Updated upstream
-=======
-
->>>>>>> Stashed changes
