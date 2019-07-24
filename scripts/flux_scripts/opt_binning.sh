ee215987ad604143684f6dcf36b37a84bfd5e590
Luke Pickering <luketpickering@googlemail.com>
2019-07-24 14:51:19 -0500

    updates binning script

--- scripts/flux_scripts/opt_binning.sh
+++ scripts/flux_scripts/opt_binning.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-NFILES=30
-NEVEQuiv=10000
+NFILES=50
+NEVEQuiv=5000
 NUSTUB=/pnfs/dune/persistent/users/picker24/nominal_5E8POT_wppfx/v3r5p4/QGSP_BERT/OptimizedEngineeredNov2017Review/nubar/dk2nulite
 NUBARSTUB=/pnfs/dune/persistent/users/picker24/nominal_5E8POT_wppfx/v3r5p4/QGSP_BERT/OptimizedEngineeredNov2017Review/nubar/dk2nulite
 
@@ -15,10 +15,9 @@ ls ${NUBARSTUB}/g4*.root | head -${NFILES} | sed "s|/pnfs/dune|root://fndca1.fna
 for hc in nu nubar; do
   for i in 14,numu -14,numubar 12,nue -12,nuebar; do
     PDG=$(echo ${i} | cut -d , -f 1)
-    NAME=$(echo ${i} | cut -d , -f 1)
+    NAME=$(echo ${i} | cut -d , -f 2)
 
     if ! dp_BuildFluxes --fhicl build_ND_fluxes_${hc}mode_dummybin_offaxis.fcl -i ${hc}.files.list --only-pdg ${PDG} --optimized-binning-file ${hc}.${NAME}.optbin.fcl --optimized-binning-N ${NEVEQuiv} --optimized-binning-min-width 0.02; then exit 1; fi
 
   done
 done
-
